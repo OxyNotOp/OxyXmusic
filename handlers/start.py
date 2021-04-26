1b00e2346294effc20eb58cea32a50954e884845
Oxy <71930942+OxyNotOp@users.noreply.github.com>
2021-04-26 14:18:28 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -32,7 +32,7 @@ async def start(_, message: Message):
             [ 
                 [
                     InlineKeyboardButton(
-                        "📜 cσммαη∂s 📜", url="https://telegra.ph/%F0%9D%95%90%F0%9D%96%94%F0%9D%96%9A%F0%9D%96%97---%F0%9D%95%AF%F0%9D%96%86%F0%9D%96%89%F0%9D%96%89%F0%9D%95%AA-%EA%97%84-04-23-5")
+                        "📜 cσммαη∂s 📜", url="https://telegra.ph/%F0%9D%95%90%F0%9D%96%94%F0%9D%96%9A%F0%9D%96%97---%F0%9D%95%AF%F0%9D%96%86%F0%9D%96%89%F0%9D%96%89%F0%9D%95%AA-%EA%97%84-04-26")
                   ],[
                     InlineKeyboardButton(
                         "❤️ αвσυт σху ❤️", url="https://t.me/aboutoxy"
