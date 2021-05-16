5b091e799d6e0f5d7e39f99fd70bc06b9f504125
Your Daddy <71930942+OxyNotOp@users.noreply.github.com>
2021-05-16 20:41:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,6 +24,7 @@ que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/a76
 admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
@@ -31,5 +32,5 @@ API_HASH = getenv("API_HASH")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
-
+ARQ_API_KEY = getenv("ARQ_API_KEY", "VKSSQH-SRNJYZ-MSOJAI-QALESR-ARQ")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
